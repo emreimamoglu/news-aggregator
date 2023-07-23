f2481e7653f01f77283881b4a97141eade316316
emreimamoglu <imamogluemree@gmail.com>
2023-07-23 21:27:00 +0300

    fix(api) : add article published at column to migration

--- backend/database/migrations/2023_07_23_134444_create_articles_table.php
+++ backend/database/migrations/2023_07_23_134444_create_articles_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('content');
             $table->string('url');
             $table->string('image_url');
+            $table->string('published_at');
             $table->foreignId('source_id')->references('id')->on('sources')->onDelete('cascade');
             $table->foreignId('author_id')->references('id')->on('authors')->onDelete('cascade');
             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
