d07f4d425b3b526adcf644192d029e3580b57622
emreimamoglu <imamogluemree@gmail.com>
2023-09-04 11:35:51 +0200

    feat(ui) : add my news and read later page uis

--- frontend-new/src/views/Home/index.tsx
+++ frontend-new/src/views/Home/index.tsx
@@ -1,11 +1,43 @@
+import Searchbar from '../../components/Searchbar';
+import { useViewport } from '../../hooks/useViewport';
+import filterIcon from '../../assets/filter.svg';
 import styles from './styles.module.scss';
+import ArticleList from '../../components/ArticleList';
 
 const Home = () => {
+
+    const { width } = useViewport();
+
     return (
         <div className={styles.container}>
-            asdas
+            {width && width > 835 && <div className={styles.header}>
+                <div className={styles.searchAndImage}>
+                    <div className={styles.search}>
+                        <Searchbar />
+                    </div>
+                    {
+                        false ? (<></>) : (
+                            <div className={styles.imgPlaceholder}>
+                                EI
+                            </div>
+                        )
+                    }
+                </div>
+            </div>}
+            <div className={styles.content}>
+                <div className={styles.title}>
+                    <h1>My News</h1>
+                    <div className={styles.searchAndFilter}>
+                        {width && width < 836 && <Searchbar />}
+                    </div>
+                </div>
+                <div className={styles.news}>
+                    <ArticleList/>
+                </div>
+
+            </div>
         </div>
-    );
+    )
 };
 
 export default Home;
--- frontend-new/src/views/ReadLater/index.tsx
+++ frontend-new/src/views/ReadLater/index.tsx
@@ -1,6 +1,41 @@
+import Searchbar from '../../components/Searchbar';
+import { useViewport } from '../../hooks/useViewport';
+import styles from './styles.module.scss';
+import ArticleList from '../../components/ArticleList';
+
 const ReadLater = () => {
-    return(
-        <>Read Later</>
+
+    const { width } = useViewport();
+
+    return (
+        <div className={styles.container}>
+            {width && width > 835 && <div className={styles.header}>
+                <div className={styles.searchAndImage}>
+                    <div className={styles.search}>
+                        <Searchbar />
+                    </div>
+                    {
+                        false ? (<></>) : (
+                            <div className={styles.imgPlaceholder}>
+                                EI
+                            </div>
+                        )
+                    }
+                </div>
+            </div>}
+            <div className={styles.content}>
+                <div className={styles.title}>
+                    <h1>Saved Articles</h1>
+                    <div className={styles.searchAndFilter}>
+                        {width && width < 836 && <Searchbar />}
+                    </div>
+                </div>
+                <div className={styles.news}>
+                    <ArticleList/>
+                </div>
+
+            </div>
+        </div>
     )
 };
 
