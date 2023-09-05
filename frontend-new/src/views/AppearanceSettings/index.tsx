0ff3c5161a40c0937aa78e64478769a8c1a92034
emreimamoglu <imamogluemree@gmail.com>
2023-09-05 15:08:08 +0200

    feat(ui) : apperance settings ui

--- frontend-new/src/views/AppearanceSettings/index.tsx
+++ frontend-new/src/views/AppearanceSettings/index.tsx
@@ -1,7 +1,45 @@
+import classNames from 'classnames';
+import minusIcon from '../../assets/minus.svg';
+import plusIcon from '../../assets/plus.svg';
+import styles from './styles.module.scss';
+
 const AppearanceSettings = () => {
     return (
-        <div>
-        AppearanceSettings
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <h1>Appearance</h1>
+            </div>
+            <div className={styles.themeSelection}>
+                <p>Color scheme</p>
+                <div className={styles.themes}>
+                    <div className={styles.light} />
+                    <div className={styles.dark} />
+                </div>
+                <p>background color</p>
+            </div>
+            <div className={styles.fontSelection}>
+                <p>Letter Size</p>
+
+                <div className={styles.selectionContainer}>
+                    <div className={styles.button}>
+                        <div className={styles.text}>
+                            <p>A</p>
+                            <p>Smaller</p>
+                        </div>
+                        <img src={minusIcon} alt='minus' />
+                    </div>
+                    <div className={styles.percentage}><p>100%</p></div>
+                    <div className={styles.button}>
+                        <img src={plusIcon} alt='plus' />
+                        <div className={classNames(styles.text,{
+                            [styles.large]: true
+                        })}>
+                            <p>A</p>
+                            <p>Larger</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
