0205155286f28f8089a162955dc09c2948f8b243
emreimamoglu <imamogluemree@gmail.com>
2023-09-06 10:44:06 +0200

    refactor(ui): add routing to topbar items

--- frontend-new/src/components/Topbar/index.tsx
+++ frontend-new/src/components/Topbar/index.tsx
@@ -1,21 +1,32 @@
+import { Route, useNavigate } from 'react-router-dom';
 import menuIcon from '../../assets/menu.svg';
 import BrandIcon from '../BrandIcon';
 import styles from './styles.module.scss';
+import { Routes } from '../../routes';
 
 export interface TopbarProps {
     toggleSidebar: () => void
 };
 
 const Topbar = ({ toggleSidebar }: TopbarProps) => {
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate(Routes.HOME);
+    };
+
+    const navigateToSettings = () => {
+        navigate(Routes.SETTINGS);
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.topbar}>
                 <img className={styles.menuIcon} src={menuIcon} alt="menu" onClick={toggleSidebar}/>
-                <div className={styles.brandIcon}>
+                <div className={styles.brandIcon} onClick={navigateToHome}>
                     <BrandIcon />
                 </div>
-                <button className={styles.profile}>
+                <button className={styles.profile} onClick={navigateToSettings}>
                     EI
                 </button>
             </div>
