13059b34b532b3208e9f159291b787f8607778b4
emreimamoglu <imamogluemree@gmail.com>
2023-09-07 11:35:04 +0200

    chore(ui) : add react-query

--- frontend-new/src/main.tsx
+++ frontend-new/src/main.tsx
@@ -6,11 +6,15 @@ import { router } from './routes'
 import "./styles/reset.scss";
 import "./styles/globals.scss";
 import { ViewportProvider } from './contexts/ViewportContext';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './config/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ViewportProvider>
-      <RouterProvider router={router} />
-    </ViewportProvider>
+    <QueryClientProvider client={queryClient}>
+      <ViewportProvider>
+        <RouterProvider router={router} />
+      </ViewportProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
