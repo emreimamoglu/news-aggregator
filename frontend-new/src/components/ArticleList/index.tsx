a90afcc5acc65b6c4de25e2009fc5777cc1e45a8
emreimamoglu <imamogluemree@gmail.com>
2023-09-19 23:13:18 +0200

    refactor(ui) : update article list component

--- frontend-new/src/components/ArticleList/index.tsx
+++ frontend-new/src/components/ArticleList/index.tsx
@@ -1,15 +1,13 @@
+import { ArticleListProps } from '../../types/Article';
 import ArticleListItem from '../ArticleListItem';
 import styles from './styles.module.scss';
 
-const ArticleList = () => {
+const ArticleList = ({articles} : ArticleListProps) => {
 
-    const article = {
-        title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt odio earum quisquam sint omnis quos magni eligendi nihil distinctio molestias."
-    }
     return (
         <div className={styles.articleList}>
-            {Array.from({ length: 30 }).map((_, index) => (
-                <ArticleListItem article={article} key={index}/>
+            {articles.map((article) => (
+                <ArticleListItem article={article} key={article.id}/>
             ))}
         </div>
     )
