80656942ecbb22e297ca312204d3eceecd19ff14
emreimamoglu <imamogluemree@gmail.com>
2023-09-22 11:06:33 +0200

    feat(ui) : connect subscription list apis

--- frontend-new/src/components/SubscriptionList/index.tsx
+++ frontend-new/src/components/SubscriptionList/index.tsx
@@ -1,17 +1,35 @@
+import { Category, Source } from '../../types/Article';
+import classnames from 'classnames';
 import styles from './styles.module.scss';
 
-const SubscriptionList = () => {
+export interface ExtendedSource extends Source{
+    isSubscribed: boolean
+}
+
+export interface ExtendedCategory extends Category{
+    isSubscribed: boolean
+}
+
+type SubscriptionListProps = {
+    data: ExtendedSource[] | ExtendedCategory[]
+    subscribeFn: (id: string, subscribed : boolean) => Promise<void>
+}
+
+const SubscriptionList = ({ data, subscribeFn }: SubscriptionListProps) => {
+    
     return (
         <div className={styles.subscriptions}>
             {
-                Array.from({ length: 30 }).map((_, index) => (
-                    <div key={index} className={styles.subscription}>
+                data.map((item) => (
+                    <div key={item.id} className={classnames(styles.subscription,{
+                        [styles.subscribed]: item.isSubscribed
+                    })}>
                         <div className={styles.info}>
-                            <p>The Verge</p>
+                            <p>{item.name}</p>
                             <p>theverge.com</p>
                         </div>
                         <div className={styles.button}>
-                            <button>Subscribe</button>
+                            <button onClick={() => subscribeFn(item.id,item.isSubscribed)}>{item.isSubscribed ? 'Unsubscribe' : 'Subscribe'}</button>
                         </div>
                     </div>
                 ))
