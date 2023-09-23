402aa93f0036e152a9f498b295b5334fd99da000
emreimamoglu <imamogluemree@gmail.com>
2023-09-23 22:49:06 +0200

    refactor(ui) : update article subscription list

--- frontend-new/src/components/SubscriptionList/index.tsx
+++ frontend-new/src/components/SubscriptionList/index.tsx
@@ -1,38 +1,53 @@
 import { Category, Source } from '../../types/Article';
 import classnames from 'classnames';
 import styles from './styles.module.scss';
+import { useState } from 'react';
+import LoadingSpinner from '../LoadingSpinner';
 
-export interface ExtendedSource extends Source{
+export interface ExtendedSource extends Source {
     isSubscribed: boolean
 }
 
-export interface ExtendedCategory extends Category{
+export interface ExtendedCategory extends Category {
     isSubscribed: boolean
 }
 
 type SubscriptionListProps = {
     data: ExtendedSource[] | ExtendedCategory[]
-    subscribeFn: (id: string, subscribed : boolean) => Promise<void>
+    subscribeFn: (id: string, subscribed: boolean) => Promise<void>
 }
 
 const SubscriptionList = ({ data, subscribeFn }: SubscriptionListProps) => {
-    
     return (
         <div className={styles.subscriptions}>
             {
-                data.map((item) => (
-                    <div key={item.id} className={classnames(styles.subscription,{
-                        [styles.subscribed]: item.isSubscribed
-                    })}>
-                        <div className={styles.info}>
-                            <p>{item.name}</p>
-                            <p>theverge.com</p>
+                data.map((item) => {
+                    const [loading, setLoading] = useState(false);
+                    return (
+                        <div key={item.id} className={classnames(styles.subscription, {
+                            [styles.subscribed]: item.isSubscribed,
+                            [styles.loading]: loading
+                        })}>
+                            <div className={styles.info}>
+                                <p>{item.name}</p>
+                                <p>theverge.com</p>
+                            </div>
+                            <div className={styles.button}>
+                                <button onClick={async() => {
+                                    try {
+                                        setLoading(true);
+                                        await subscribeFn(item.id, item.isSubscribed)
+                                        setLoading(false);
+                                    } catch (error) {
+                                        console.log(error);
+                                    }
+                                }}>
+                                    {loading ? <LoadingSpinner width={15} height={15} color='#FFF'/> : item.isSubscribed ? "Unsubscribe" : "Subscribe"}
+                                </button>
+                            </div>
                         </div>
-                        <div className={styles.button}>
-                            <button onClick={() => subscribeFn(item.id,item.isSubscribed)}>{item.isSubscribed ? 'Unsubscribe' : 'Subscribe'}</button>
-                        </div>
-                    </div>
-                ))
+                    )
+                })
             }
         </div>
     )
