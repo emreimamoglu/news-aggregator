e44f199c1c55f594d1cab6e30af99c52bc43cb09
emreimamoglu <imamogluemree@gmail.com>
2023-09-23 23:25:30 +0200

    refactor(ui) : filter onclicks

--- frontend-new/src/components/Chip/index.tsx
+++ frontend-new/src/components/Chip/index.tsx
@@ -4,12 +4,15 @@ import classnames from 'classnames';
 interface ChipProps {
     label: string;
     selected?: boolean;
+    onClick?: () => void;
 }
-const Chip = ({ label, selected }: ChipProps) => {
+const Chip = ({ label, selected, onClick }: ChipProps) => {
     return (
         <div className={classnames(styles.chip, {
             [styles.selected]: selected
-        })}>
+        })}
+            onClick={onClick}
+        >
             <p>{label}</p>
         </div>
     )
