384bad619dfb13182c9f7e34e0cb61a2e0e8a6df
emreimamoglu <imamogluemree@gmail.com>
2023-09-27 00:15:29 +0200

    refactor(ui) : add header navigation

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,28 +2,38 @@ import styles from './styles.module.scss';
 import Searchbar from '../Searchbar';
 import classnames from 'classnames';
 import { useViewport } from '../../hooks/useViewport';
+import { useNavigate } from 'react-router-dom';
+import { Routes } from '../../routes';
 
 interface HeaderProps {
     searchFn?: (searchTerm: string) => void;
 }
 
+
 const Header = ({ searchFn }: HeaderProps) => {
     const { width } = useViewport();
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate(Routes.SETTINGS);
+    };
 
     return (
         <div className={(styles.header)}>
-            <div className={classnames(styles.searchAndImage,{
+            <div className={classnames(styles.searchAndImage, {
                 [styles.noSearch]: !searchFn
             })}>
                 {searchFn && <div className={styles.search}>
-                    <Searchbar searchFn={searchFn}/>
+                    <Searchbar searchFn={searchFn} />
                 </div>}
-                { width && width > 835 && 
-                    (false ? (<></>) : (
-                        <div className={styles.imgPlaceholder}>
-                            EI
-                        </div>
-                    ))
+                {width && width > 835 &&
+                    <div onClick={handleLogoClick}>
+                        {(false ? (<></>) : (
+                            <div className={styles.imgPlaceholder}>
+                                EI
+                            </div>
+                        ))}
+                    </div>
                 }
             </div>
         </div>
