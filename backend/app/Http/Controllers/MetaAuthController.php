9e9d3403050012bea7f800a8eddbcf84cf24205d
emreimamoglu <imamogluemree@gmail.com>
2023-10-08 21:11:52 +0200

    feat(api) : complete meta oauth2 login functions

--- backend/app/Http/Controllers/MetaAuthController.php
+++ backend/app/Http/Controllers/MetaAuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
@@ -13,8 +14,8 @@ class MetaAuthController extends Controller
     function redirectToMeta()
     {
         $clientId = getenv('META_CLIENT_ID');
-        $url = "https://www.facebook.com/v18.0/dialog/oauth?client_id={$clientId}&redirect_uri=http://localhost:5173/auth/meta&state=state&scope=email";
-        
+        $url = "https://www.facebook.com/v18.0/dialog/oauth?client_id={$clientId}&redirect_uri=http://localhost:5173/auth/meta&state=statexddx&scope=email";
+
         \Log::info('User redirecting to Meta for authentication');
         return $this->success([
             'url' => $url
@@ -23,27 +24,47 @@ class MetaAuthController extends Controller
 
     function handleMetaCallback()
     {
-        $clientId = getenv('META_CLIENT_ID');
-        $clientSecret = getenv('META_CLIENT_SECRET');
-        $code = request()->code;
-        $client = new \GuzzleHttp\Client();
+        try {
+            $clientId = getenv('META_CLIENT_ID');
+            $clientSecret = getenv('META_CLIENT_SECRET');
+            $code = request()->code;
+            $client = new \GuzzleHttp\Client();
 
-        $response = $client->request('GET', "https://graph.facebook.com/v18.0/oauth/access_token?client_id={$clientId}&redirect_uri=http://localhost:5173/auth/meta&client_secret={$clientSecret}&code={$code}");
-        $token = json_decode($response->getBody()->getContents())->access_token;
+            $response = $client->request('GET', "https://graph.facebook.com/v18.0/oauth/access_token?client_id={$clientId}&redirect_uri=http://localhost:5173/auth/meta&client_secret={$clientSecret}&code={$code}");
+            $token = json_decode($response->getBody()->getContents())->access_token;
 
-        $data = $client->get('https://graph.facebook.com/v18.0/me', [
-            'headers' => [
-                'Accept' => 'application/json',
-            ],
-            'query' => [
-                'fields' => 'id,name,email,picture',
-                'access_token' => $token,
-            ]
-        ]);
-        \Log::info('User authenticated with Meta: ' . json_decode($data->getBody()->getContents())->email);
-        return $this->success([
-            'token' => $token,
-            'user' => json_decode($data->getBody()->getContents()),
-        ], null, 200);
+            $data = $client->get('https://graph.facebook.com/v18.0/me', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+                'query' => [
+                    'fields' => 'id,name,email,picture',
+                    'access_token' => $token,
+                ]
+            ]);
+
+            $contents = $data->getBody()->getContents();
+            $decoded = json_decode($contents);
+
+            /** @var User $user */
+            $user = User::query()
+                ->updateOrCreate(
+                    ['email' => $decoded->email],
+                    [
+                        'name' => $decoded->name,
+                        'password' => null,
+                        'meta_id' => $decoded->id,
+                    ]
+                );
+
+            \Log::info('User authenticated with Meta: ' . $contents);
+            return $this->success([
+                'token' => $user->createToken('API_TOKEN')->plainTextToken,
+                'user' => $user,
+            ], null, 200);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return $this->error(null, 'Error authenticating with Meta', 500);
+        }
     }
 }
